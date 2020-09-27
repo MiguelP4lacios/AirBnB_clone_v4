0bfb344259433d7f7b9641ec1556c1cc85ae1058
Miguel Palacios <oswaldo9605@gmail.com>
2020-09-27 07:06:53 +0000

    [FIX]:Arrow function is changed to anonymous function, html → is changed to text and this is used to reference each amenity.
    On branch feature_miguel
    Your branch is up to date with 'origin/feature_miguel'.
    
    Changes to be committed:
    	modified:   web_dynamic/static/scripts/1-hbnb.js
    	modified:   web_dynamic/templates/1-hbnb.html

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,15 +1,15 @@
 $(document).ready(() => {
   const id = [];
   const name = [];
-  $('.amenities INPUT').click(() => {
+  $('.amenities INPUT').click(function () {
     if ($(this).prop('checked') === true) {
-      id.push($('.amenities INPUT').data('id'));
-      name.push($(this).parents('LI').html());
-      $('.amenities H4').html(name);
+      id.push($(this).data('id'));
+      name.push($(this).parents('LI').text());
+      $('.amenities H4').text(name);
     } else if ($(this).prop('checked') === false) {
-      id.pop($('.amenities INPUT').removeData('id'));
-      name.pop($(this).parents('LI').html());
-      $('.amenities H4').html(name);
+      id.pop($(this).removeData('id'));
+      name.pop($(this).parents('LI').text());
+      $('.amenities H4').text(name);
     }
   });
 });
